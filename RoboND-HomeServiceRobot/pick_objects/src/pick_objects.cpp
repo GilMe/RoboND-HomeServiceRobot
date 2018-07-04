81ab9f7e75271e37daad3fdb1c631a2bc34735a5
Gil Meiri <gil.meiri@gmail.com>
2018-07-04 17:25:22 +0300

    feat: add pick objects package and shell script

---
+++ RoboND-HomeServiceRobot/pick_objects/src/pick_objects.cpp
@@ -0,0 +1,93 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+#define PICKUP_X 5.0
+#define PICKUP_Y -8.5
+#define PICKUP_O 1.0
+
+#define DROPOFF_X 0.0
+#define DROPOFF_Y -8.5
+#define DROPOFF_O 2.0
+
+
+// Define a client for to send goal requests to the move_base server through a SimpleActionClient
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "pick_objects");
+
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  // Wait 5 sec for move_base action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  move_base_msgs::MoveBaseGoal goal;
+
+  //*********************
+  // For pickup
+  //*************
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = PICKUP_X;
+  goal.target_pose.pose.position.y = PICKUP_Y;
+  goal.target_pose.pose.orientation.w = PICKUP_O;
+
+   // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+
+  ROS_INFO("Robot is travelling to the pickup zone");
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
+    ROS_INFO("Robot reached pickup zone");
+    ROS_INFO("Robot picking up virtual object");
+    ros::Duration(5).sleep(); // sleep for 5 seconds
+    ROS_INFO("Robot picked up virtual object");
+  }
+  else
+    ROS_ERROR("The base failed to reach pickup zone for some reason");
+
+ //*********************
+  // For dropoff
+  //*************
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = DROPOFF_X;
+  goal.target_pose.pose.position.y = DROPOFF_Y;
+  goal.target_pose.pose.orientation.w = DROPOFF_O;
+
+   // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+
+  ROS_INFO("Robot is travelling to the dropoff zone");
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Robot dropped off virtual object");
+  else
+    ROS_ERROR("The base failed to reach dropoff zone for some reason");
+
+  
+  return 0;
+}
+
