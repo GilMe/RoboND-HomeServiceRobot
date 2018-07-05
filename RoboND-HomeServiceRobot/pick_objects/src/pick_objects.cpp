899fd7ff06059628e06534bfd73cd7ce627745f6
Gil Meiri <gil.meiri@gmail.com>
2018-07-05 07:56:11 +0300

    feat: add goal status publisher to pick_objects

--- RoboND-HomeServiceRobot/pick_objects/src/pick_objects.cpp
+++ RoboND-HomeServiceRobot/pick_objects/src/pick_objects.cpp
@@ -2,6 +2,8 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
+#include "std_msgs/String.h"
+
 #define PICKUP_X 5.0
 #define PICKUP_Y -8.5
 #define PICKUP_O 1.0
@@ -18,8 +20,14 @@ int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
   ros::init(argc, argv, "pick_objects");
 
-  //tell the action client that we want to spin a thread by default
-  MoveBaseClient ac("move_base", true);
+  // Create a ROS NodeHandle object
+  ros::NodeHandle n;
+
+  // create a goal publisher topic
+  ros::Publisher goal_status_publisher = n.advertise<std_msgs::String>("goal_status", 4);;
+
+  //add node handle and tell the action client that we want to spin a thread by default
+  MoveBaseClient ac(n,"move_base", true);
 
   // Wait 5 sec for move_base action server to come up
   while(!ac.waitForServer(ros::Duration(5.0))){
@@ -27,6 +35,7 @@ int main(int argc, char** argv){
   }
 
   move_base_msgs::MoveBaseGoal goal;
+  std_msgs::String goal_status_msg;
 
   //*********************
   // For pickup
@@ -45,6 +54,10 @@ int main(int argc, char** argv){
   ROS_INFO("Sending goal");
   ac.sendGoal(goal);
 
+  // publish status msg
+  goal_status_msg.data = "Moving towards pickup";
+  goal_status_publisher.publish(goal_status_msg);
+
   ROS_INFO("Robot is travelling to the pickup zone");
   // Wait an infinite time for the results
   ac.waitForResult();
@@ -54,8 +67,15 @@ int main(int argc, char** argv){
   {
     ROS_INFO("Robot reached pickup zone");
     ROS_INFO("Robot picking up virtual object");
+
+    // publish status msg
+    goal_status_msg.data = "Reached pickup";
+    goal_status_publisher.publish(goal_status_msg);
+
+    //wait 5 seconds for pickup
     ros::Duration(5).sleep(); // sleep for 5 seconds
     ROS_INFO("Robot picked up virtual object");
+
   }
   else
     ROS_ERROR("The base failed to reach pickup zone for some reason");
@@ -77,13 +97,24 @@ int main(int argc, char** argv){
   ROS_INFO("Sending goal");
   ac.sendGoal(goal);
 
+
+  // publish status msg
+  goal_status_msg.data = "Moving towards dropoff";
+  goal_status_publisher.publish(goal_status_msg);
+
   ROS_INFO("Robot is travelling to the dropoff zone");
   // Wait an infinite time for the results
   ac.waitForResult();
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
+    // publish status msg
+    goal_status_msg.data = "Reached dropoff";
+    goal_status_publisher.publish(goal_status_msg);
+
     ROS_INFO("Robot dropped off virtual object");
+  }
   else
     ROS_ERROR("The base failed to reach dropoff zone for some reason");
 
